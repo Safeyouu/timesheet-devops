b0fc969c22612ee8385f959d72f5e0d5c8bef21c
Safeyouu <youssef.chahab@esprit.tn>
2023-10-06 11:08:25 +0100

    we just added a comment

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -24,7 +24,7 @@ public class User implements Serializable {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
-	
+	//This is a comment
 	private String firstName; 
 	
 	private String lastName;
